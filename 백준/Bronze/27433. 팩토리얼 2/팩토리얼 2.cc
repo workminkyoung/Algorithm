33b6182e4052e727db45af4ca33741540a326cd2
workminkyoung <147801724+workminkyoung@users.noreply.github.com>
2024-10-13 21:25:56 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -1,23 +1,31 @@
 #include <iostream>
 using namespace std;
 
-long long Factorial(int n){
-    if(n == 1){
-        return 1;
-    }
-    else if(n == 0){
-        return 1;
-    }
-    else{
-        return n * Factorial(n-1);
-    }
+long long result = 0;
+
+long long factorial(int num) {
+	if (num == 0)
+		return 1;
+	else 
+	{
+		result = num * factorial(num - 1);
+		return result;
+	}
 }
 
-int main(int argc, const char * argv[]) {
-    int n = 0;
-    cin >> n;
-    
-    cout << Factorial(n);
-    
-    return 0;
+int main()
+{
+	int n;
+	cin >> n;
+
+	if (n == 0) {
+		result = 1;
+	}
+	else {
+		factorial(n);
+	}
+
+	cout << result;
+
+	return 0;
 }
